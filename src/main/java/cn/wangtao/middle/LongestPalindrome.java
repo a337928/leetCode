ccd9551d76ae97b202d106adfba78b361dbbf041
a337928 <swongtao@163.com>
2020-08-07 22:58:45 +0800

    Z convert

---
+++ src/main/java/cn/wangtao/middle/LongestPalindrome.java
@@ -0,0 +1,14 @@
+package cn.wangtao.middle;
+
+public class LongestPalindrome {
+	public static void main(String[] args){
+
+		System.out.println(solution("ABCDEFGHIJKLMN"));
+
+	}
+
+
+	public static String solution(String s) {
+		return null;
+	}
+}
