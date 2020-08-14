251437bb40d7e5856b515bf01f88a6bf56fbfe32
a337928 <swongtao@163.com>
2020-08-14 22:36:50 +0800

    add Reverse

--- src/main/java/cn/wangtao/easy/Palindrome.java
+++ src/main/java/cn/wangtao/easy/Palindrome.java
@@ -7,13 +7,17 @@ public class Palindrome {
 	}
 
 	private static Boolean isPalindrome(int x){
+
+		if (x < 0 || ( x % 10 == 0 && x != 0)){
+			return Boolean.FALSE;
+		}
+
 		int result = 0;
-		while (x != 0){
-			int pop = x % 10;
-			result = result * 10 + pop;
+		while (x > result){
+			result = x % 10 + result * 10;
 			x = x / 10;
 
 		}
-		return result == x;
+		return result == x || x == result / 10;
 	}
 }
