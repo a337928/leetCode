cc2145b0281459449d0104514aed3ad8bddd22c1
a337928 <swongtao@163.com>
2020-09-12 22:26:46 +0800

    add romanToInt

--- src/main/java/cn/wangtao/easy/IsValid.java
+++ src/main/java/cn/wangtao/easy/IsValid.java
@@ -1,5 +1,6 @@
 package cn.wangtao.easy;
 
+import javax.management.remote.rmi._RMIConnection_Stub;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -14,30 +15,40 @@ public class IsValid {
 			return false;
 		}
 
-		Map<Character, Character> pairs = new HashMap<Character, Character>() {{
-			put(')', '(');
-			put(']', '[');
-			put('}', '{');
-		}};
 		Deque<Character> stack = new LinkedList<>();
 		for (int i = 0; i < n; i++) {
-			char ch = s.charAt(i);
-			if (pairs.containsKey(ch)) {
-				if (stack.isEmpty() || !stack.peek().equals(pairs.get(ch))) {
-					return false;
-				}
-				stack.pop();
-			} else {
-				stack.push(ch);
+			char right  = getRight(s.charAt(i));
+			if (right != 0) {
+				stack.push(right);
+				continue;
 			}
+			if (stack.isEmpty() || !stack.peek().equals(s.charAt(i))){
+				return false;
+			} else {
+				stack.pop();
+			}
+
 		}
 		return stack.isEmpty();
 
 	}
 
+	private char getRight(char left){
+		switch (left){
+			case 40 :
+				return (char)41;
+			case 91 :
+				return  (char)93;
+			case 123 :
+				return  (char)125;
+			default:
+				return 0;
+		}
+	}
+
 
 	public static void main(String[] args){
 		IsValid isValid = new IsValid();
-		System.out.println(isValid.isValid("[{()]}"));
+		System.out.println(isValid.isValid("{}[]()"));
 	}
 }
