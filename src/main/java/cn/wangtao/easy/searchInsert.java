04fc4e0366f68c5e8b36e573b41bc56a4f2e770d
a337928 <swongtao@163.com>
2020-09-24 23:02:50 +0800

    add searchInsert

--- src/main/java/cn/wangtao/easy/searchInsert.java
+++ src/main/java/cn/wangtao/easy/searchInsert.java
@@ -3,13 +3,20 @@ package cn.wangtao.easy;
 public class searchInsert {
 	public int searchInsert(int[] nums, int target) {
 
-		for(int i = 0; i < nums.length; i ++){
-			if (target <= nums[i]){
-				return i;
+		int left = 0;
+		int right = nums.length - 1;
+		int result = nums.length;
+		while (left <= right){
+			int mid = left + ((right - left) >> 1);
+			if (target <= nums[mid]){
+				result = mid;
+				right = mid - 1;
+			} else {
+				left = mid + 1;
 			}
-
 		}
-		return nums.length;
+
+		return result;
 
 	}
 }
