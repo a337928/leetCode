47b6dfb8c964e5e0f40a258ef2e5d1fb421042d4
a337928 <swongtao@163.com>
2020-11-28 23:09:22 +0800

    add levelOrderBottom

--- src/main/java/cn/wangtao/easy/LevelOrderBottom.java
+++ src/main/java/cn/wangtao/easy/LevelOrderBottom.java
@@ -1,12 +1,36 @@
 package cn.wangtao.easy;
 
-import java.util.List;
+import java.util.*;
 
 public class LevelOrderBottom {
 
 	public List<List<Integer>> levelOrderBottom(TreeNode root) {
-		return null;
+		List<List<Integer>> result = new LinkedList<>();
+		if (root == null){
+			return result;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		while (!queue.isEmpty()){
+			List<Integer> level = new ArrayList<>();
+			int count = queue.size();
+			for (int i = 0; i < count; i ++){
+				TreeNode node = queue.poll();
+				level.add(node.val);
+				if (node.left != null){
+					queue.offer(node.left);
+				}
+				if (node.right != null){
+					queue.offer(node.right);
+				}
+			}
+			result.add(0, level);
+		}
+
+		return result;
 	}
+
+
 
 	public class TreeNode {
 		int val;
