f96c4a95610260eea857532de6f27ade6b27303f
a337928 <swongtao@163.com>
2020-12-03 22:56:21 +0800

    add IsBalanced

---
+++ src/main/java/cn/wangtao/easy/IsBalanced.java
@@ -0,0 +1,27 @@
+package cn.wangtao.easy;
+
+import java.util.Objects;
+
+public class IsBalanced {
+
+	public boolean isBalanced(TreeNode root) {
+		return recur(root) != -1;
+	}
+
+	private int recur(TreeNode root) {
+		if (Objects.isNull(root)){
+			return 0;
+		}
+		int left = recur(root.left);
+		if (left == -1){
+			return -1;
+		}
+
+		int right = recur(root.right);
+		if (right == -1 ){
+			return -1;
+		}
+
+		return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+	}
+}
