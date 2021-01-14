4e138fdfb73ae6cf409872c6185ad9dc4da9bf6c
a337928 <swongtao@163.com>
2021-01-14 22:46:34 +0800

    add MaxProfit 2

--- src/main/java/cn/wangtao/easy/MaxProfit.java
+++ src/main/java/cn/wangtao/easy/MaxProfit.java
@@ -24,4 +24,21 @@ public class MaxProfit {
 		System.out.println(maxProfit.maxProfit(p));
 
 	}
+
+
+	public int maxProfit2(int[] prices) {
+		if (prices == null){
+			return 0;
+		}
+
+		int profit = 0;
+		for (int i = 1; i < prices.length; i ++){
+			if (prices[i] > prices[i - 1]){
+				profit += prices[i] - prices[i - 1];
+			}
+		}
+
+		return profit;
+
+	}
 }
